4414d84df702e3a1d4e0ca717162a56c0a5a3c12
arenaasd <akgamerz397@gmail.com>
2025-05-12 01:46:55 +0500

    fix save

--- app/dashboard/resume/_components/Form/ExperienceDetails.jsx
+++ app/dashboard/resume/_components/Form/ExperienceDetails.jsx
@@ -114,6 +114,7 @@ const ExperienceDetails = ({ enableNext }) => {
       const cleanExperience = experienceList.map(({ id, ...rest }) => {
         return {
           ...rest,
+          endDate: rest.endDate?.trim() ? rest.endDate : 'Present',
           summary: rest.summary || '' // Ensure summary is never null or undefined
         };
       });
@@ -251,7 +252,7 @@ const ExperienceDetails = ({ enableNext }) => {
             type="button" 
             onClick={onSave} 
             disabled={saving}
-            className="w-full xs:w-auto mt-2 sm:mt-0"
+            className="w-32 mt-2 sm:mt-0"
             size="sm"
           >
             {saving ? <LoaderCircle className="animate-spin mr-2" size={16} /> : null}
