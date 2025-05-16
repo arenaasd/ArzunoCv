5601cf9270225c3a13c1f1060e7ca585fd2b2d32
arenaasd <akgamerz397@gmail.com>
2025-05-17 00:29:20 +0500

    done

--- app/page.jsx
+++ app/page.jsx
@@ -14,22 +14,18 @@ function Home() {
           ArzunoCV
         </h1>
         <p className=" mt-16 text-2xl">Build a professional ATS-friendly resume or CV quickly and effortlessly — no design skills required.</p>
-        <motion.div
-          initial={{ opacity: 0, y: 100, scale: 0 }}
-          whileInView={{ opacity: 1, y: 0, scale: 1 }}
-          exit={{ opacity: 0, y: 100, scale: 0 }}
-          transition={{
-            duration: 0.4,
-            ease: "easeOut",
-          }}
-          className="flex flex-col items-center gap-4  mt-10">
+        <motion.div 
+        initial={{ opacity: 0, y: 100, scale:0 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        exit={{ opacity: 0, y: 100, scale:0 }}
+         transition={{
+                duration: 0.4,
+                ease: "easeOut",
+              }}
+        className="flex flex-col items-center gap-4  mt-10">
           <Link href="/dashboard" className="w-full max-w-xs">
-            <Button className="animated-upgrade-button w-full text-2xl py-6 flex items-center justify-center gap-3 bg-gradient-to-b from-[#eea34e] to-[#5A4192]">
-              Get Started
-              <ArrowRight style={{ width: "24px", height: "24px" }} />
-            </Button>
+            <Button className="w-full text-2xl py-6 flex items-center justify-center gap-3 bg-gradient-to-b from-[#eea34e] to-[#5A4192]">Get Started<ArrowRight style={{ width: "24px", height: "24px" }}/></Button>
           </Link>
-
           <Button className="w-full max-w-xs text-2xl py-6 flex items-center justify-center gap-3"><Video style={{ width: "24px", height: "24px" }} />Demo</Button>
         </motion.div>
       </div>
@@ -39,7 +35,7 @@ function Home() {
         </div>
       </div>
       <div className="mt-16 p-10">
-        <FaqSection />
+      <FaqSection />
       </div>
     </>
   );
